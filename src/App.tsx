3ab518e54cb630551edd2102b2086e8fe6927e1c
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-07-07 19:58:33 -0300

    Added NewRoom component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
 
+import { NewRoom } from "./pages/NewRoom";
 import "./styles/global.scss";
 
 export default function App() {
-  return <Home />;
+  return <NewRoom />;
 }
