ec18af3e2bdd454272b264a95f650172b03b9ba5
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-07-07 19:46:32 -0300

    Added Button Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
 export function Home() {
   return (
@@ -25,7 +26,8 @@ export function Home() {
           <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
-            <button type="submit">Entrar na sala</button>
+
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
