cf1dbcacf57f9f6ab0e222cd8960521ce23a9567
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-07-13 20:49:18 -0300

    Added RoomCode component

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,21 @@
+import copyImg from "../assets/images/copy.svg";
+import "../styles/roomCode.scss";
+
+interface RoomCodeProps {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
+  return (
+    <button className="roomCode" onClick={copyRoomCodeToClipboard}>
+      <div>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{props.code}</span>
+    </button>
+  );
+}
