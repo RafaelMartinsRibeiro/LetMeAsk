505249c8d22bb40a66649c3e63e647cbdc4d35dc
Rafael Martins <raff@pop-os.localdomain>
2022-07-12 22:28:01 -0300

    Added handleJoinRoom in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
@@ -8,11 +9,13 @@ import "../styles/auth.scss";
 
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { Room } from "./Room";
+import { database } from "../services/firebase";
 
 export function Home() {
   const navigate = useNavigate();
-
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function handleCreateRoom() {
     if (!user) {
@@ -20,6 +23,22 @@ export function Home() {
     }
 
     navigate("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
   }
 
   return (
@@ -40,8 +59,13 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
 
             <Button type="submit">Entrar na sala</Button>
           </form>
